3b310eac5696a246b14ea1cb90419a6211052676
Hugo Bullón <hugobullont@gmail.com>
2020-06-14 19:31:04 -0500

    web

---
+++ precache-manifest.dbd65b70e3b106559e864e8132575dc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115a8003c32cca5a26124bc8bd3f549f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38ac4a53251b75b08333",
+    "url": "/static/css/main.dae92b9d.chunk.css"
+  },
+  {
+    "revision": "5b6adf6df8ccb8c8c3aa",
+    "url": "/static/js/2.b42a9b19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b42a9b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ac4a53251b75b08333",
+    "url": "/static/js/main.8b782019.chunk.js"
+  },
+  {
+    "revision": "94910d619befd5713b46",
+    "url": "/static/js/runtime-main.588902e2.js"
+  },
+  {
+    "revision": "3473bb88a4b0f5d77980cf1ec0eb257a",
+    "url": "/static/media/logo.3473bb88.svg"
+  }
+]);
